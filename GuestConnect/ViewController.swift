977eebfd49633424b4be5cd2290f7d69de82f9c2
Richard Sunden <djrap1dtecno@gmail.com>
2019-12-07 19:49:05 -0500

    Initial Commit

---
+++ GuestConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuestConnect
+//
+//  Created by Richard Sunden on 12/7/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
